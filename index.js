1fdd8cd2f5fde19c9e9612750cce3dfec366c965
Haldwing <139230101+Haldwing@users.noreply.github.com>
2023-09-07 14:35:03 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+$(".biolink").on("mouseover", function(){
+ $(".biolink").animate({opacity: 1})
+});
+
+$(".biolink").on("mouseout", function(){
+    $(".biolink").animate({opacity: 0.7})
+   });
+   
